11eaea5801334c3fe5892c85e140ba972192358f
Trachifab <fabiendorey@gmail.com>
2017-02-13 17:52:54 +0100

    Add Firebase configuration to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDlqxdk5ay3gfP9sO8qvhZSFwoRhDSzWD8",
+    authDomain: "a2-firebase-84a3d.firebaseapp.com",
+    databaseURL: "https://a2-firebase-84a3d.firebaseio.com",
+    storageBucket: "a2-firebase-84a3d.appspot.com",
+    messagingSenderId: "443664095016"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
